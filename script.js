d8ab9f3e0fb9115fa0ffa7f37df65ae7b3d56c3b
ra-403 <146449633+ra-403@users.noreply.github.com>
2023-11-05 06:25:00 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////
+// Make Mobile Navigation Work
+const btnNavEl = document.querySelector(".Mobile-nav");
+const linksEl = document.querySelector(".navbar-container");
+
+btnNavEl.addEventListener("click", function () {
+  linksEl.classList.toggle("nav-open");
+});
+// //////////////////////////////////////////////////////////
+// // Make Mobile Navigation Work
+// const btnNavEl = document.querySelector(".Mobile-nav");
+// const navbarEl = document.querySelector(".navbar-container");
+
+// btnNavEl.addEventListener("click", function () {
+//   navbarEl.classList.toggle("nav-open");
+// });
